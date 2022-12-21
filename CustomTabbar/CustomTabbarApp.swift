5b8674bb571983cc048047b1b34b5be83daf1e3c
mrsafadi2 <mrsafadi2@gmail.com>
2022-12-21 11:54:57 +0200

    Initial Commit

---
+++ CustomTabbar/CustomTabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabbarApp.swift
+//  CustomTabbar
+//
+//  Created by Mohammed Safadi Macbook Pro on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
